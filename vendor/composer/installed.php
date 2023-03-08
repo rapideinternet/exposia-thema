a380003effd1422923294dbd2bb16cd1fd45182a
rick-visser <rick@rapide.software>
2023-03-08 10:02:14 +0100

    v1.0.2 - theme reconstruction
    Removed all the dutch and made it more user friendly

---
+++ vendor/composer/installed.php
@@ -0,0 +1,44 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rapidesoftware/tailwind-theme-theme',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8598c8b6806dde0e0930160d865f67419a8c81a6',
+        'type' => 'october-theme',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'v1.12.0',
+            'version' => '1.12.0.0',
+            'reference' => 'd20a64ed3c94748397ff5973488761b22f6d3f19',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'rapidesoftware/tailwind-theme-theme' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8598c8b6806dde0e0930160d865f67419a8c81a6',
+            'type' => 'october-theme',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'roundcube/plugin-installer' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'shama/baton' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+    ),
+);
